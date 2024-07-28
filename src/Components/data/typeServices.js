05c9def4abbd93bc50bceee2934a629372fc8237
LAPTOP-L7H0H0EL\glez_ <Jesus_Glez>
2024-07-27 18:27:51 -0600

    Version donde se quita soldadura y se tiene que visualizar el log

--- src/Components/data/typeServices.js
+++ src/Components/data/typeServices.js
@@ -6,12 +6,6 @@ export const typeServices=[
     'description': 'Ofrecemos una amplia gama de servicios de maquinado convencional, utilizando tornos, fresadoras, y otras herramientas especializadas para transformar y ajustar piezas metálicas según sus especificaciones. Ideal para proyectos que requieren precisión y adaptabilidad a diferentes tamaños y formas.'
   },
   {
-    'id': 'control-numerico',
-    'name': 'Centros de Maquinado',
-    'type': 'Control numérico',
-    'description': 'Nuestros centros de maquinado con control numérico permiten realizar procesos complejos con alta precisión y eficiencia. Estos equipos están diseñados para trabajar con una variedad de materiales y cumplir con los requisitos más exigentes de cada proyecto.'
-  },
-  {
     'id': 'soldadura',
     'name': 'Servicios de Soldadura',
     'type': 'Especializada',
